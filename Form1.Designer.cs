bae6df057f3193943b81c6a82bb822fa007efc6a
Fenskolo <fenskolo@gmail.com>
2017-03-10 01:46:13 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace nauka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menadzer = new System.Windows.Forms.ListView();
+            this.nazwa = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.waga = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.typ = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 126);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(771, 24);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.menadzer);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(480, 24);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(291, 102);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.richTextBox1);
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 126);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(771, 432);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 536);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(771, 22);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel6.Location = new System.Drawing.Point(514, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(257, 432);
+            this.panel6.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(514, 432);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseClick);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menadzer
+            // 
+            this.menadzer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nazwa,
+            this.waga,
+            this.typ});
+            this.menadzer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menadzer.FullRowSelect = true;
+            this.menadzer.HideSelection = false;
+            this.menadzer.Location = new System.Drawing.Point(0, 0);
+            this.menadzer.Name = "menadzer";
+            this.menadzer.Size = new System.Drawing.Size(291, 102);
+            this.menadzer.TabIndex = 0;
+            this.menadzer.UseCompatibleStateImageBehavior = false;
+            this.menadzer.View = System.Windows.Forms.View.Details;
+            // 
+            // nazwa
+            // 
+            this.nazwa.Text = "nazwa";
+            this.nazwa.Width = 150;
+            // 
+            // waga
+            // 
+            this.waga.Text = "waga";
+            // 
+            // typ
+            // 
+            this.typ.Text = "typ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 558);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView menadzer;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ColumnHeader nazwa;
+        private System.Windows.Forms.ColumnHeader waga;
+        private System.Windows.Forms.ColumnHeader typ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace nauka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] nazwy;
+            nazwy = Directory.GetFiles(@"C:\Users\Tosia\Downloads");
+            FileInfo plik;
+            string nazwapliku;
+            ListViewItem mm;
+            string bez;
+            decimal waga;
+          //  ListView zz;
+
+            int i;
+            for (i = 0; i < nazwy.Length; i++)
+            {
+                plik = new FileInfo(nazwy[i]);
+                nazwapliku = Path.GetFileNameWithoutExtension(nazwy[i]);
+                    
+                bez = plik.Extension;
+                waga =Math.Round(Convert.ToDecimal(plik.Length) / 1048576,2);
+
+                mm = new ListViewItem(new string[] { nazwapliku,waga.ToString(),bez });
+                menadzer.Items.Add(mm);
+            }
+            
+         //   for(i=0; i<nazwy.Length; i++)
+
+
+          //      bez = plik.Extension;
+               
+           //     menadzer.Items[2].SubItems.Add("dupa");
+                //menadzer.Items.Add(dysk);
+                //menadzer.Items.Clear();
+               
+            
+         //   menadzer.SelectedIndex = menadzer.Items.IndexOf("C:\\");
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text = menadzer.SelectedItems[0].Text;
+        }
+    }
+}
